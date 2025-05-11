16e31ec0e7750a031723dd411514e42c4a93eb59
Dawid Pawliczek <dawidpawliczek4@gmail.com>
2025-05-11 11:07:35 +0200

    switch to hash router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import AppRoutes from './routes'
 import { CartProvider } from '../cart/providers/CartContext'
 
 
 const App = () => {
   return (
-    <BrowserRouter basename="/Dawid_Pawliczek_Web_Wroclaw">
+    <HashRouter basename="/Dawid_Pawliczek_Web_Wroclaw">
       <CartProvider>
         <AppRoutes />
       </CartProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
